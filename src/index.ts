007705793a92b0aa0343ced5e6bea03aab00ca98
zacanger <zac@zacanger.com>
2023-09-24 02:20:50 -0600

    fix: use ts-standard

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { gzip, gunzip } from 'zlib';
-import { promisify } from 'util';
-import { Buffer } from 'buffer';
+import { gzip, gunzip } from 'zlib'
+import { promisify } from 'util'
+import { Buffer } from 'buffer'
 
-const gz = promisify(gzip);
-const ugz = promisify(gunzip);
+const gz = promisify(gzip)
+const ugz = promisify(gunzip)
 
 const compress = async (s: string = ''): Promise<string> => {
-  const compressed = await gz(s);
-  return Buffer.from(compressed).toString('base64');
-};
+  const compressed = await gz(s)
+  return Buffer.from(compressed).toString('base64')
+}
 
 const decompress = async (s: string = ''): Promise<string> => {
-  const decompressed = await ugz(Buffer.from(Buffer.from(s, 'base64')));
-  return decompressed.toString();
-};
+  const decompressed = await ugz(Buffer.from(Buffer.from(s, 'base64')))
+  return decompressed.toString()
+}
 
 export = {
   compress,
-  decompress,
-};
+  decompress
+}
